a0a9979f18b012e1764a62d9229b8fa4efcd3685
Vinicius Monteiro <vinicius.monteiro@altaservicosmedicos.com.br>
2025-10-24 15:23:13 -0300

    teste 4

--- treino_githubactions/test_soma.py
+++ treino_githubactions/test_soma.py
@@ -1,6 +1,12 @@
-import soma as s
+import soma as som
+import subtracao as sub
 
 def test_soma():
-    result = s.soma(2, 2)
+    result = som.soma(2, 2)
 
     assert 4 == result
+
+def test_subtracao():
+    result = sub.subtracao(2, 2)
+
+    assert 0 == result
